ed98c8ea392ae285b02bdf7b2f7659f3d788db9d
xenn <rishi734438@gmail.com>
2024-07-24 19:34:47 +0530

    in memory db

--- flask_task/utils/environment.py
+++ flask_task/utils/environment.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///sites.db")
+DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///:memory:")
 TEMPLATES_AUTO_RELOAD = os.environ.get("TEMPLATES_AUTO_RELOAD", True)
